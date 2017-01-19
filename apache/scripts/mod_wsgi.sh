4ab72fee694745d6f8ce1904f9198da2f968b94e
ismaelresp <ismaelresp@telefonica.net>
2017-01-19 11:29:40 +0100

    updated apache2 config for mdsrv

--- apache/scripts/mod_wsgi.sh
+++ apache/scripts/mod_wsgi.sh
@@ -1,6 +1,6 @@
-wget https://github.com/GrahamDumpleton/mod_wsgi/archive/4.4.21.tar.gz
+wget https://github.com/GrahamDumpleton/mod_wsgi/archive/4.5.13.tar.gz
 tar xfz 4.4.21.tar.gz
-cd mod_wsgi-4.4.21
+cd mod_wsgi-4.5.13
 ./configure --with-python=/usr/local/bin/python3
 make
 make install
