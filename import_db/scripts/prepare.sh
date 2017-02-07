a9459fdfdcf1bf817ea1df040f3472bfc50b2173
Ismael Rodriguez Espigares <ismaelresp@telefonica.net>
2017-02-07 17:25:55 +0100

    fix bug in import_db/prepare.sh

--- import_db/scripts/prepare.sh
+++ import_db/scripts/prepare.sh
@@ -1,4 +1,4 @@
-LAST_DIR = "$(pwd -P)"
+LAST_DIR="$(pwd -P)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)"
 psql -U protwis -h localhost -o /dev/null protwis < "$DIR/prepare.sql" 2>/dev/null
 cd "$LAST_DIR"
